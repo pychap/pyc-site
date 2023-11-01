c707ad77f93adc26d206c3415dda60560daa6fff
Peter Chapman <pchapman@ptc.com>
2023-11-01 13:19:09 -0700

    PostCSS now works, hero tune-up

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,24 +1,18 @@
-
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    reqquire('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('postcss-custom-media'),
-    require('postcss-preset-env')({
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss': {},
+    'postcss-custom-media': {},
+    'postcss-preset-env': {
       stage: 1,
-      features: {
+      features: { 
+        'nesting-rules': false,
         'custom-selectors': { preserve: true }
       },
       env: 'development',
-    }),
-    require('postcss-jit-props')(openProps),
-  ]
+    },
+    'postcss-jit-props': 'openProps',
+    'autoprefixer': {},
+  }
 }
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   }
-// }
