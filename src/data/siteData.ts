ea35b3e09280096ae8904d3ab7d5961165b1d564
Peter Chapman <pychapman22@gmail.com>
2024-03-18 13:56:45 -0700

    Fix footer, add relative paths, other fixes.

---
+++ src/data/siteData.ts
@@ -0,0 +1,32 @@
+export const baseData = {
+  title: "Peter Chapman's Website",
+  description: "Welcome to my website. Hopefully you'll find this resourceful and enlightening.",
+};
+
+export const socialData = [
+  {
+    title: "github",
+    href: "https://github.com/pychap",
+    icon: "icon--github",
+  },
+  {
+    title: "discord",
+    href: "https://discord.com/",
+    icon: "icon--discord",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/pychap",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://www.youtube.com/@PeterChapman-py",
+    icon: "icon--youtube",
+  },
+  {
+    title: "instagram",
+    href: "https://instagram.com/pychap",
+    icon: "icon--instagram",
+  },
+];
