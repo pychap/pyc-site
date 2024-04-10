c1bd9053a5c27de31c3d20c03693204b63d1c716
Peter Chapman <pychapman22@gmail.com>
2024-04-10 16:26:07 -0700

    Prep for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import purgecss from "astro-purgecss";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://py-chapman.netlify.app/",
-  integrations: [tailwind(), icon(), purgecss(), sitemap()]
+  integrations: [tailwind(), icon(), purgecss(), sitemap()],
+  output: "hybrid",
+  adapter: netlify()
 });
