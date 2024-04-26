c943f59f9b0cfc573d4e5dd150cfb8eb8bb5ef31
Peter Chapman <pychapman22@gmail.com>
2024-04-26 13:52:28 -0700

    Bringing images into MDx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://py-chapman.netlify.app/",
-  integrations: [tailwind(), icon(), sitemap()],
+  integrations: [tailwind(), icon(), sitemap(), mdx()],
   output: "hybrid",
   adapter: netlify()
 });
