74e4862a004012ca0cf1d73501d5038092f98ca7
Peter Chapman <pychapman22@gmail.com>
2024-05-09 17:18:16 -0700

    Design tweaks, add Chat column blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,27 @@ export default defineConfig({
   site: "https://py-chapman.netlify.app/",
   integrations: [tailwind(), icon(), sitemap(), mdx()],
   output: "hybrid",
-  adapter: netlify()
+  adapter: netlify(),
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'dracula',
+      // Alternatively, provide multiple themes
+      // See note below for using dual light/dark themes
+      experimentalThemes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
 });
